46291555a3e42f9bafc736e5d18e90c4a4d5daa1
anifaaulia <115767144+anifaaulia@users.noreply.github.com>
2025-02-21 10:20:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from components.Header import *
 from components.Main import *
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
 def main():
 
     Header()
     Main()
 
-if _name_ == "_main_":
-    main()
+if name == "main":
+    main()
